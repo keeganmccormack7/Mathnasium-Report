9a2dfc4118725b09a73f19fef74b2fcf94d16e98
keeganmccormack7 <73142845+keeganmccormack7@users.noreply.github.com>
2020-10-19 19:14:51 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+#Keegan McCormack
+#Compare and Contrast Premier League Players from the 2019/2020 season
+print("Premier League Comparisons")
+KDBG=13
+#Kevin De Bruyne Goals
+KDBA=20
+#Kevin De Bruyne Assists
+KDBG+KDBA=KDBC
+#Kevin De Bruyne Chances Created
+MSG=19
+#Mohammed Salah Goals
+MSA=10
+#Mohammed Salah Assists
+MSG+MSA=MSC
+#Mohammed Salah Chances Created
+SHMA=10
+#Son Hueng Min Assists
+SHMG=11
+#Son Hueng Min Goals
+SHMG+SHMA=SHMC
+#SHMC=Son Hueng Min Chances created
+JVG=23
+#Jamie Vardy Goals
+JVA=5
+#Jamie Vardy Assists
+JVA+JVG=JVC
+#JVC= Jamie Vardy Chances created
+KDBG>JVG
+#False
+KDBA>MSA
+#True
+If KDBA>=10 print("Great season")
+else print("Needs Improvement")
+If MSA>=10 print("Great season")
+else print("Needs improvement)
+If SHMA>=10 print("Great Season")
+else print("Needs Improvement")
+If JVA>=10 print("Great Season")
+else print("Needs Improvement")
+If KDBG>=23 print("Winner of Golden Boot")
+elif KDBG>=15 print("Great Season")
+else KDBG<15 print("Good Season")
+If MSG>=23 print("Winner of Golden Boot")
+elif MSG>=15 print("Great Season")
+else MSG<15 print("Good Season")
+If JVG>=23 print("Winner of Golden Boot")
+elif JVG>=15 print("Great Season")
+else JVG<15 print("Good Season")
+If SHMG>=23 print("Winner of Golden Boot")
+elif SHMG>=15 print("Great Season")
+else SHMG<15 print("Good Season")
